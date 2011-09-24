6c213134a7eb86ed468c640646e69a78b6c13101
Antony Jones <ant@bronx.(none)>
2011-09-24 16:02:31 +0100

    Adding release plugin config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.release.scm.enabled=false
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
