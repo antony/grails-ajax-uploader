0c0644a2ce5620813adfdca283f9a1e3ef5af5d4
Antony Jones <aj@debtonator.io>
2014-06-09 13:24:27 +0100

    New release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,9 +22,9 @@ grails.project.dependency.resolution = {
     }
     plugins {
 
-        build ':release:2.0.4', {
-            export = false
-        }
+	build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+      		export = false
+   	}
 
     }
 }
